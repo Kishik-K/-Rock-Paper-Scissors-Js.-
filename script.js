a3bbf83207b45dae1775dcb7082a77a8269cdda1
Kishik-K <kishikkhajuria03@gmail.com>
2026-02-22 23:27:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function playRock() {
+  const randomNumber = Math.random();
+  let cpuMove = '';
 
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+    cpuMove = 'ROCK';
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    cpuMove = 'PAPER';
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+    cpuMove = 'SCISSORS';
+  }
+
+  let result = '';
+
+  if (cpuMove === 'ROCK') {
+    result = 'TIE';
+  } else if (cpuMove === 'PAPER') {
+    result = 'YOU LOSE';
+  } else if (cpuMove === 'SCISSORS') {
+    result = 'YOU WIN';
+  }
+
+  document.getElementById('result').textContent =
+    'You picked ROCK — CPU picked ' + cpuMove + ' — ' + result;
+}
