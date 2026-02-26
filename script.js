b45e2af5c96e7e03eafcee29680c69c060d52c28
Kishik-K <kishikkhajuria03@gmail.com>
2026-02-26 16:13:41 +0530

    Update script.js
    a final update of functions

--- script.js
+++ script.js
@@ -26,8 +26,7 @@ function playRock() {
     result = 'YOU WIN';
   }
 
-  document.getElementById('result').textContent =
-    'You picked ROCK — CPU picked ' + cpuMove + ' — ' + result;
+  showResult('ROCK', cpuMove, result);
 }
 
 function playPaper() {
@@ -53,4 +52,29 @@ function playPaper() {
   }
 
   showResult('PAPER', cpuMove, result);
+}
+
+function playScissors() {
+  const randomNumber = Math.random();
+  let cpuMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+    cpuMove = 'ROCK';
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    cpuMove = 'PAPER';
+  } else if (randomNumber >= 2/3 && randomNumber < 1) {
+    cpuMove = 'SCISSORS';
+  }
+
+  let result = '';
+
+  if (cpuMove === 'ROCK') {
+    result = 'YOU LOSE';
+  } else if (cpuMove === 'PAPER') {
+    result = 'YOU WIN';
+  } else if (cpuMove === 'SCISSORS') {
+    result = 'TIE';
+  }
+
+  showResult('SCISSORS', cpuMove, result);
 }
